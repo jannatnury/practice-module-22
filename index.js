8c936e4b1b295fdefb950d5b8a6e5930384314bc
jannatnury <jannatnury253@gmail.com>
2022-02-04 02:44:32 +0600

    task 6 done

--- index.js
+++ index.js
@@ -8,10 +8,18 @@ function addition(num1, num2) {
 }
 let add = addition(234, 566);
 console.log(add);
-var number = 76;
+var number = 7776;
 var number2 = 768;
 let additionOfNumber = number + number2;
 console.log("Addition is:", additionOfNumber);
+if (number2 > number) {
+    console.log("Number-2 is big");
+}
+else { console.log("Number is large"); }
+
+if (number2 != number) {
+    console.log("number & number2 is not equal ");
+}
 //  Subtraction
 let number3 = 679;
 let number4 = 769;
@@ -31,3 +39,12 @@ let float = parseFloat(division);
 console.log("Float of division:", float);
 const modulation = number5 % number6;
 console.log("Modulation is :", modulation);
+
+let numb = 79;
+let numb2 = 342;
+if (numb > numb2 && numb < 80) {
+    console.log(numb);
+}
+else if (numb2 < numb || numb2 > 100) {
+    console.log(numb2);
+}
